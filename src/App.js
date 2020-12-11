9d81b6fc1a3b473d4edfca7c3f6ada9ca687475c
volosevych <vestynkas@gmail.com>
2020-12-10 23:35:29 -0700

    removed serviceWorker file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Main from './components/Main';
 import Wrapper from './components/Wrapper/wrapper';
 import Header from './components/Header/header';
-import "./App.css";
 
 function App() {
   return (
